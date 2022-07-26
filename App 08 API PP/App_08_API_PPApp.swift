c67b84016563238eaf31fcb4559e96ceb9cffa3a
Lina Bhatia <lina@bhatia.org>
2022-07-26 13:19:50 -0500

    Initial Commit

---
+++ App 08 API PP/App_08_API_PPApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_08_API_PPApp.swift
+//  App 08 API PP
+//
+//  Created by Lina Bhatia on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct App_08_API_PPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
